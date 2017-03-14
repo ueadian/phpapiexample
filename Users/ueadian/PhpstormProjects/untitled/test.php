d1f1f37fb969ccdb710cefc88fb340f0e8a5d655
brandon <brandon.hawkins@maritzcx.com>
2017-03-13 20:46:50 -0600

    commented

--- Users/ueadian/PhpstormProjects/untitled/test.php
+++ Users/ueadian/PhpstormProjects/untitled/test.php
@@ -1,14 +1,21 @@
 <?php
+/*
+ * Function to get articles, startindex is the article to start at, count is how many to return, default is 0 10
+ */
 function getArticles($startIndex = 0, $count = 10){
+    // open up the api url with startindex and count put in
     $req = curl_init("http://ign-apis.herokuapp.com/articles?startIndex=" . $startIndex . "&count=" . $count);
+    // set curl to return a response and to use GET
     curl_setopt($req, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($req, CURLOPT_HTTPGET, true);
-
+    // tell the app we are application / json (not necesary)
     curl_setopt($req, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/json',
         'Accept: application/json'
     ));
+    //exec the curl
     $resp = curl_exec($req);
+    //decode the json into an array
     return json_decode($resp, true);
 }
 ?>
@@ -26,10 +33,14 @@ function getArticles($startIndex = 0, $count = 10){
     <body>
         <?php
             $respJ = getArticles();
+            //data is where all the articles are, so foreach through each one
             foreach($respJ["data"] as $article)
             {
+                //grab the headline from metadata and put it in a header tag
                 echo("<h1>" . $article["metadata"]["headline"] . "</h1>");
+                //add a line break
                 echo("<br />");
+                // get the first img (compact) url and use it in an img tag
                 echo("<img src=\"" . $article["thumbnails"][0]["url"] . "\">");
             }
 
